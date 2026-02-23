ac697e022099eb6ab65d9185669d817862d97c80
starppqq463-create <starppqq463@gmail.com>
2026-02-24 02:08:43 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -20,7 +20,7 @@
   // 고래 추적 설정
   const WHALE_THRESHOLD_USD = 2000; // 100만 달러 이상 감지
   const PROXY_CANDIDATES = [
-    url => '/api/proxy?url=' + encodeURIComponent(url), // Vercel 서버리스 함수
+    url => '/api/proxy?url=' + encodeURIComponent(url), // Vercel 서버리스 함수 (최우선)
     url => 'https://corsproxy.io/?' + encodeURIComponent(url),
     url => 'https://api.allorigins.win/get?url=' + encodeURIComponent(url),
     url => 'https://thingproxy.freeboard.io/fetch/' + url,
@@ -28,8 +28,7 @@
   ];
   // POST 요청을 지원하는 프록시 목록
   const POST_SUPPORTING_PROXIES = [
-    PROXY_CANDIDATES[0], // Vercel API
-    PROXY_CANDIDATES[1]  // corsproxy.io (백업)
+    PROXY_CANDIDATES[0] // Vercel API
   ];
 
   async function fetchProxy(url, options = {}) {
